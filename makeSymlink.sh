e14600ab1e2abda3a1f28d51d2a2f4418b19c40a
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2015-10-11 19:33:30 +0200

    backup of screen-batstatus screenrc and vimrc files

---
+++ makeSymlink.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+############################
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+############################
+
+########## Variables
+
+dir=~/dotfiles                    # dotfiles directory
+olddir=~/dotfiles_old             # old dotfiles backup directory
+files="screenrc vimrc screen-batstatus"    # list of files/folders to symlink in homedir
+
+##########
+
+# create dotfiles_old in homedir
+mkdir -p $olddir
+
+# change to the dotfiles directory
+cd $dir
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    mv ~/.$file ~/dotfiles_old/ >/dev/null
+    ln -s $dir/$file ~/.$file
+done
+echo "done"
