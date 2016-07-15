e0432fc32a8b92345b3df0af05743af6a83cbedc
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2016-07-15 12:03:08 +0200

    changed the printout of linkSnippets.sh

--- linkSnippets.sh
+++ linkSnippets.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 if [ -d "../.vim/bundle/ultisnips"  ]; then
-	echo it is there
 	rm -rf ~/.vim/bundle/ultisnips/UltiSnips
 	ln -s ~/dotfiles/UltiSnips ~/.vim/bundle/ultisnips/UltiSnips
+	echo snippets linked
 else
-	echo its not there
+	echo you must install ultisnips first
 fi
