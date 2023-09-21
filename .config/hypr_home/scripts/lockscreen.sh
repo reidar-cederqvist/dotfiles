7eb86e966a713d2447340ebf966f0b43a75bc561
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2023-09-21 23:40:54 +0200

    hyprland: start on a config for hyprland

---
+++ .config/hypr_home/scripts/lockscreen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+
+monitors=$(hyprctl -j monitors | jq -r .[].name)
+active_window_address=$(hyprctl -j activewindow | jq -r .address)
+
+echo $monitors
+
+for monitor in $monitors; do
+	hyprctl dispatch focusmonitor $monitor
+	alacritty -o window.startup_mode=Fullscreen -e bash -c "cmatrix -ab -C cyan" &
+	pids="$pids $!"
+	sleep 0.2
+done
+
+swaylock -c 00000000
+
+for pid in $pids; do
+	kill $pid
+done
+
+hyprctl dispatch focuswindow address:$active_window_address
